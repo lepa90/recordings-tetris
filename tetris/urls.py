b1af988fab35eecffd9c9eb41974c78637374247
Kamil Przylepa <kamil.przylepa@gmail.com>
2025-12-09 18:33:33 +0100

    Dodano strone glowna, menu nawigacyjne i podstrone O nas

--- tetris/urls.py
+++ tetris/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.tetris_view, name='tetris'),
+    path('', views.home_view, name='home'),
+    path('tetris/', views.tetris_view, name='tetris'),
+    path('o-nas/', views.about_view, name='about'),
 ]
--- tetris/views.py
+++ tetris/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 
 
+def home_view(request):
+	return render(request, 'tetris/home.html')
+
+
 def tetris_view(request):
 	return render(request, 'tetris/index.html')
+
+
+def about_view(request):
+	return render(request, 'tetris/about.html')
